cb14d3b2c6e8fcf89c0d1d405eef8f703db83a75
Vikas Rajoria <vrajoria@walmartlabs.com>
2017-12-22 01:25:46 +0530

    message service as cache

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,30 @@ import { Injectable } from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 
+import {Observable} from 'rxjs/Observable';
+import {of} from 'rxjs/observable/of';
+
+import {MessageService} from './message.service';
+
 @Injectable()
 export class HeroService {
 
-  constructor() { }
-
+  constructor(private messageService: MessageService) { }
+  
+  result: Observable<Hero[]>;
   /**
    * Return all heroes.
    */
-  getHeroes() : Hero[] {
-    return HEROES;
+  getHeroes() : Observable<Hero[]> {
+    this.messageService.add("Calling Hero Fetch service");
+    try {
+      this.result = of(HEROES);
+    } catch (err) {
+      this.messageService.add("Exception while calling Hero service");
+    }
+    this.messageService.add("Hero fetch call successful");
+    
+    return this.result;
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,8 +19,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  getHeroes () {
-    this.heroes = this.heroService.getHeroes();
+  getHeroes (): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect (hero: Hero) {
